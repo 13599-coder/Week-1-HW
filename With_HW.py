d972f080368ee22b0e602084fdcbcbbf3df2dc88
EvanM <13599@edu.wheatlandhigh.org>
2026-01-11 18:48:27 -0800

    Update With_HW.py
    We now close the minutes file

--- With_HW.py
+++ With_HW.py
@@ -3,5 +3,7 @@ with open("step_counter",'w') as minutes_file:
   for minute in range(1, user_minutes + 1):
     minutes = input('#{num} what would you like to add to your mintues file? : ')
     minutes_file.write(f"{minutes}\n")
-    
+  mintues_file.close()
+
+  
     
