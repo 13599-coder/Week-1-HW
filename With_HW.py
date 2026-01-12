1fbddba5263a3718d5a3492bc3326e954947b0ce
EvanM <13599@edu.wheatlandhigh.org>
2026-01-11 18:53:09 -0800

    Update With_HW.py
    I forgot to close the user_minutes

--- With_HW.py
+++ With_HW.py
@@ -1,4 +1,4 @@
-user_minutes = int(input("How many minutes do you want to enter?")
+user_minutes = int(input("How many minutes do you want to enter?"))
 with open("step_counter",'w') as minutes_file:
   for minute in range(1, user_minutes + 1):
     minutes = input('#{num} what would you like to add to your mintues file? : ')
