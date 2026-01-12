7f9f52d3a9b319f4747dde7d6468f0ab01a81b7d
EvanM <13599@edu.wheatlandhigh.org>
2026-01-11 18:46:57 -0800

    Update With_HW.py
    We first asked the user how many minutes they would like to enter in their step counter. Then we use one with statement to handle both writing and reading. Lastly I made a loop that asks how many minute value they would like to add.

--- With_HW.py
+++ With_HW.py
@@ -1,26 +1,7 @@
-user_years = int(input('Enter the amount of years you would like to add: '))
-file_variable = open('example_file.txt','w')
-  for num in range(1, num_lines + 1):
-entry = input('#{num} What would you like to add to the file? ')
-file_variable.write(f'{entry}\n')
-file_variable.close
-file_object = open('example.txt','r')
-  for line in file_object:
-print(line)
-file_object.close
-num_list= open('num_list.txt', 'r')
-line = num_list.readline()
-sum = 0
-while line != '':
-    sum += int(line)
-    line = num_list.readline()
-num_list.close()
-print(f'The sum of the numbers in the list is {sum}')
-file_object = open('example.txt', 'r')
-for line in file_object:
-    print(line)
-  file_objecct.close()
-with open(filename, mode) as file_variable
-  for number in range(2)
-    ourfile.write(f'{number}\n')
-with open(filename1,mode1) as file_varible1, open(filename2,mode2) as file_varible2
+user_minutes = int(input("How many minutes do you want to enter?")
+with open("step_counter",'w') as minutes_file:
+  for minute in range(1, user_minutes + 1):
+    minutes = input('#{num} what would you like to add to your mintues file? : ')
+    minutes_file.write(f"{minutes}\n")
+    
+    
