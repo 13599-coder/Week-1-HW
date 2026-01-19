32ec4f74214101fd7b0c044c6ce539e53b403491
EvanM <13599@edu.wheatlandhigh.org>
2026-01-18 19:27:46 -0800

    Update calculator.py
    added a try and expect for division and fixed the error on line 29

--- calculator.py
+++ calculator.py
@@ -6,23 +6,27 @@ def math_ops(a, b):
   add = a + b
   sub = a - b
   mul = a * b
-  div = a / b
+  try: 
+    div = a / b
+  except:
+    div = "cannot be divided by zero"
   return add, sub, mul, div
 print('Welcome to your very basic calculator!!!!')
-operation = (input('Which operation would you like your calculator to perform? Add/Sub/Divide/Multiple: ')).lower()
-num_1 = float(input('enter the first number: '))
-num_2 = float(input('enter the second number: '))
-a, s, m, d = math_ops(num_1, num_2)
-if operation == "add":
-  print("addition:", a)
-elif operation == "sub":
-  print("subtractaion:", s)
-elif operation == "multiple":
+try:
+  operation = (input('Which operation would you like your calculator to perform? Add/Sub/Divide/Multiple: ')).lower()
+  num_1 = float(input('enter the first number: '))
+  num_2 = float(input('enter the second number: '))
+  a, s, m, d = math_ops(num_1, num_2)
+  if operation == "add":
+    print("addition:", a)
+  elif operation == "sub":
+    print("subtractaion:", s)
+  elif operation == "multiple":
     print("multiplication:", m)
-elif operation == "divide":
+  elif operation == "divide":
     print("division:", d)
-else: 
-  print("invalid operation")
+  else: 
+    print("invalid operation")
 
 file = open("calculator_file.txt, "a")
 file.write(f"{num1}, {num2}\n")
@@ -31,3 +35,7 @@ file.write(f"sub: {sub}\n")
 file.write(f"mul: {mul}\n")
 file.write(f"div: {div}\n")
 file.close()
+except ValueError:
+  print('please enter only interger values.')
+except ZeroDivisionError:
+  print("excuse me you cant divide by zero...")
